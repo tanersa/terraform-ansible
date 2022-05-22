20e13b2ef49dc31cb89ba9820d24c0bf5f1ed730
Taner Gur <tanersagur@gmail.com>
2022-05-22 17:21:07 -0400

    fixed region

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket         = "sharks-terra-backend123"
     key            = "terraform.tfstate"
-    region         = "us-east-2"
+    region         = "us-east-1"
     dynamodb_table = "terraformDynamoDB"
   }
 }
