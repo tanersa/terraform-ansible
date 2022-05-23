66ebc637a726b5a25629e0fea3230f8abd8eebde
Taner Gur <tanersagur@gmail.com>
2022-05-23 12:18:58 -0400

    fixed lock file

--- provider.tf
+++ provider.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = var.aws_region
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "sharks-tfstate-backend"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraformDynamoDB"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "sharks-tfstate-backend"
+#     key            = "terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraformDynamoDB"
+#   }
+# }
 
 # terraform state file will be stored in S3 by default
 # we did this below example for s3 only practice purpose
