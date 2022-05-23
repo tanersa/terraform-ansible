468d59c07c0d0cda8d0d21ec53100d3f5b77931e
Taner Gur <tanersagur@gmail.com>
2022-05-23 11:51:41 -0400

    fixed lock file

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "sharks-terra-backend123"
+    bucket         = "sharks-tfstate-backend"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraformDynamoDB"
